5722cdabb04d4a5ba88ac6102f3d38ac56c13819
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2025-11-29 14:25:26 +0900

    add

--- impl-stack/stack.py
+++ impl-stack/stack.py
@@ -1,23 +1,38 @@
-from dataclass import dataclass
-from typing import Self
+from dataclasses import dataclass
+from typing import Self, Any
 
 
 @dataclass
 class Node:
+    item: Any
+    next: Self | None
 
 
 class Stack:
     def __init__(self):
-        self._head = None
+        self._head: Node | None = None
 
-    def pop(self) -> any | None:
-        pass
+    def pop(self) -> Any:
+        head = self._head
+        if head is None:
+            raise IndexError()
+        self._head = head.next
+        return head.item
 
-    def push(self, item: any):
-        pass
+    def push(self, item: Any):
+        node = Node(item, None)
+        head = self._head
+        if head is None:
+            self._head = node
+            return
+        node.next = head
+        self._head = node
 
-    def peek(self) -> any | None:
-        pass
+    def peek(self) -> Any:
+        head = self._head
+        if head is None:
+            raise IndexError()
+        return head.item
 
     def isEmpty(self):
-        pass
+        return self._head is None
