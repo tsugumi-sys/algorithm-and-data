70760261cd5db63a1d7818c6aad04220c3bdec5a
Akira Noda <61897166+tsugumi-sys@users.noreply.github.com>
2025-12-18 09:49:26 +0900

    start implementing min heap

--- impl-heap/main.py
+++ impl-heap/main.py
@@ -1,8 +1,17 @@
-# Implement your heaps here
-
 class MinHeap:
     def __init__(self, values=None):
-        raise NotImplementedError
+        # https://docs.python.org/3.13/faq/programming.html#how-do-i-convert-between-tuples-and-lists
+        # If the argument is a list, it makes a copy just like seq[:] would.
+        # NOTE: For this problem, we restrict values as int or float for simplicity.
+        #   In the actual situation, heap should be generalized for any data types and comparable.
+        if values is None:
+            self._heap = []
+        else:
+            if not all(isinstance(v, int) for v in values):
+                raise TypeError("All elements must be int")
+            self._heap = list(
+                values
+            )  # create an copy to avoid modifying the given value directly.
 
     def push(self, value):
         raise NotImplementedError
